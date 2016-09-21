2c15b74911530c791f24e82b8286a5baed4b0212
klarkc <walker@praiseweb.com.br>
2016-09-21 14:17:57 -0300

    add auto-close-issues

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var loadJson = require('jsonfile').readFileSync;
 var GitHub = require('github-api');
+var conf = loadJson('conf.json');
+
+var api = new GitHub();
+var interval = conf["refresh-time"] * 1000;
+
+console.log("Welcome, the interval is", interval, "ms");
+
+setInterval(require('lib/auto-close-issues')(api), interval);
