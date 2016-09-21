072b74df59f762e70ed37af639037d277ad2e0cc
klarkc <walker@praiseweb.com.br>
2016-09-21 15:58:47 -0300

    fixed, using babel without commonjs module and official github-api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+let GitHub = require('github-api');
 let loadJson = require('jsonfile').readFileSync;
-
 let conf = loadJson('./conf.json');
 
-let api = {};
+let api = new GitHub();
 let interval = conf["refresh-time"] * 1000;
 
 console.log("Welcome, the interval is", interval, "ms");
