d8d7a5df366ce515b7a23148cb14f58096ebceb8
klarkc <walker@praiseweb.com.br>
2016-09-21 15:19:29 -0300

    add prototype

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var loadJson = require('jsonfile').readFileSync;
-var GitHub = require('github-api');
-var conf = loadJson('conf.json');
+let loadJson = require('jsonfile').readFileSync;
 
-var api = new GitHub();
-var interval = conf["refresh-time"] * 1000;
+let conf = loadJson('./conf.json');
+
+let api = {};
+let interval = conf["refresh-time"] * 1000;
 
 console.log("Welcome, the interval is", interval, "ms");
 
-setInterval(require('lib/auto-close-issues')(api), interval);
+setInterval(require('./lib/auto-close-issues')(api), interval);
