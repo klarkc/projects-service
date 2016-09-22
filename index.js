1418e3563d2ea51e0b100be087d6a04cf10a0724
klarkc <walker@praiseweb.com.br>
2016-09-22 17:12:48 -0300

    fix loggging

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 let GitHub = require('github-api');
 let loadJson = require('jsonfile').readFileSync;
-let timer = require('./lib/timer.js');
-let conf = loadJson('./conf.json');
+
 let api = new GitHub();
 
+let conf = loadJson('./conf.json');
+let interval = conf['refresh-time'];
+
+let logger = require('./lib/logger.js')(conf.log);
+
+let timer = require('./lib/timer.js');
 let autoCloseIssues = require('./lib/auto-close-issues')(api);
 
-let interval = conf["refresh-time"];
+timer(autoCloseIssues, conf.actions['auto-close-issues'], interval);
 
-console.log("\nWelcome! The refresh interval is", interval, "s");
-
-timer(autoCloseIssues, conf.actions["auto-close-issues"], interval);
+logger.info('We are ready to rock! The refresh interval is of', interval + 's');
