ccb5facfc3d042bf92b0293319cfc92d3b1e0f25
klarkc <walker@praiseweb.com.br>
2016-09-29 16:32:53 -0300

    add tests, add logger.test.js test

---
+++ test/logger.test.js
@@ -0,0 +1,45 @@
+const winston = require('winston');
+const assert = require('chai').assert;
+
+describe('logger.js module behavior', function() {
+  let conf, defLogger;
+
+  before(function() {
+    conf = {
+      "server": "info",
+      "auto-close-issues": "info",
+      "timer": "info",
+      "colorize": true
+    };
+
+    defLogger = require('../lib/logger.js')(conf);
+  });
+
+  it('should create a server logger', function() {
+    let logger = winston.loggers.get('server');
+    assert.instanceOf(logger, winston.Logger);
+    assert.deepPropertyVal(logger.transports, 'console.label', 'server');
+    assert.deepPropertyVal(logger.transports, 'console.level', conf.server);
+    assert.isFunction(logger.transports.console.timestamp);
+  });
+
+  it('should create an auto-close-issues logger', function() {
+    let logger = winston.loggers.get('auto-close-issues');
+    assert.instanceOf(logger, winston.Logger);
+    assert.deepPropertyVal(logger.transports, 'console.label', 'auto-close-issues');
+    assert.deepPropertyVal(logger.transports, 'console.level', conf.server);
+    assert.isFunction(logger.transports.console.timestamp);
+  });
+
+  it('should create a timer logger', function() {
+    let logger = winston.loggers.get('timer');
+    assert.instanceOf(logger, winston.Logger);
+    assert.deepPropertyVal(logger.transports, 'console.label', 'timer');
+    assert.deepPropertyVal(logger.transports, 'console.level', conf.server);
+    assert.isFunction(logger.transports.console.timestamp);
+  });
+
+  it('should return the default server logger', function() {
+    assert.equal(defLogger, winston.loggers.get('server'));
+  });
+});
