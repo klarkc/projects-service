5e3c343e343699d875502170900c15141b0eaf66
Walker Leite <walker@praiseweb.com.br>
2016-10-08 11:39:39 -0300

    add auto-close-issues test

---
+++ test/auto-close-issues.test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+const GitHub = require('github-api');
+const api = new GitHub();
+const autoCloseIssues = require('../lib/auto-close-issues')(api);
+
+describe('action auto-close-issues', function() {
+  it.skip('should close open issues in done column', function() {
+    // TODO: should close open issues in done column
+  });
+});
